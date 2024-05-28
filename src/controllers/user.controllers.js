0280e84b9f248e74ab243cd21003a3fd982475d5
sumit1104-max <sumitnaskar950@gmail.com>
2024-05-28 23:31:23 +0530

    login-logout

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -4,13 +4,29 @@ import { User } from "../models/user.models.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const generateAccessTokenAndRefreshToken = async(userId) => {
+    try {
+        const user = User.findById(userId)
+        const accesToken = user.generateAccessToken()
+        const refereshToken = user.generaterefreshToken() 
+
+        user.refereshToken =refereshToken
+        await user.save({ validateBeforeSave: false })
+
+        return {accesToken, refereshToken}
+
+
+    } catch (error) {
+        throw new ApiError(500, "Something Went Wrong while Generating tokens")
+    }
+}
 
 
 const registerUser = asyncHandler( async (req, res) => {
    //get user details from frontend
 
    const {fullName, email, username, password} = req.body
-   console.log("email: ", email);
+//    console.log(req.body);
 
    //validation - not empty
 //one method
@@ -19,15 +35,14 @@ const registerUser = asyncHandler( async (req, res) => {
 //    }
 
     if (
-        [fullName, email, username, password].some((field) => 
-            field?.trim() === "")
+        [fullName, email, username, password].some((field) => field?.trim() === "")
     ) {
         throw new ApiError(400, "All fields are required")
     }
 
    //check if user allready registrer: username, email
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username },{ email }]
     })
 
@@ -38,7 +53,13 @@ const registerUser = asyncHandler( async (req, res) => {
    //check img avtr
 
    const avatarLocalPath = req.files?.avatar[0]?.path;
-   const coverImageLocalPath = req.files?.coverImage[0]?.path;
+//    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+//    console.log(req.files);
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
    if (!avatarLocalPath) {
     throw new ApiError(400, "avatar file is required")
@@ -58,7 +79,8 @@ const registerUser = asyncHandler( async (req, res) => {
     fullName,
     avatar: avatar.url,
     coverImage: coverImage?.url || "",
-    email,password,
+    email,
+    password,
     username: username.toLowerCase()
    })
 
@@ -80,7 +102,74 @@ const registerUser = asyncHandler( async (req, res) => {
 
 })
 
+const loginUser = asynHandler(async (req, res) =>{
+    //req body se data
+    const {email, password, username} =req.body
+    //username or email
+    if (!(username || email)) {  
+        throw new ApiError(400, "username or email is required")
+    }
+    //find the user
+    const user = await User.findOne({
+        $or: [{username}, {email}]
+    })
+
+    if(!user){
+        throw new ApiError(400, "User does not exist")
+    }
+    //password check
+
+    const isPassValid = await user.isPasswordCorrect(password)
+
+    if(!isPassValid){
+        throw new ApiError(401, "Check Your Password")
+    }
+    // access and refresh token
+    const {accesToken, refereshToken} = await generateAccessTokenAndRefreshToken(user._id)
+
+    const loggedinUser = await User.findById(user._id).select("-password -refreshToken")
+    //send cookie
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200).cookie("accesToken", accesToken, options).cookie("refreshToken", refereshToken,options).json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedinUser, accesToken, refereshToken
+            },
+            "User logged in suucessfully"
+        )
+    )
+
+
+})
+
+const logoutUser = asynHandler(async(req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set:{
+                refereshToken: undefined
+            }
+        },
+        {
+            new:true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200).clearCookie("accesToken", options).clearCookie("refreshToken", options)
+})
 
 export { 
     registerUser,
+    loginUser,
+    logoutUser
 }
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -17,7 +17,8 @@ const uploadOnCloudinary = async (localFilePath) => {
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
-        console.log("file is uploaded successfully",response.url);
+        // console.log("file is uploaded successfully",response.url);
+        fs.unlinkSync(localFilePath)
         return response;
     } catch (error) {
         fs.unlinkSync(localFilePath)
